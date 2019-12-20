3e56b3d97376b082f6ac9832621bcda829b00b6a
Alessandro Rosetti <alessandro.rosetti@gmail.com>
2019-12-20 10:52:51 +0100

    add publish script

---
+++ publish.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+hugo -t monopriv # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+cd public
+git add .
+
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+
+git push origin master
